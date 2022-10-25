565ad28464ddfbaa71c9e9b6db87558ad8138279
ifitton <ifitton845@gmail.com>
2022-10-25 23:39:01 +0100

    Safety

--- Build.sh
+++ Build.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-git clone https://gitlab.com/libeigen/eigen.git External/eigen
-git clone https://github.com/nothings/stb.git External/stb
+
+if [ ! -d "External/eigen" ]
+then
+	git clone https://gitlab.com/libeigen/eigen.git External/eigen
+fi
+
+if [ ! -d "External/stb" ]
+then
+	git clone https://github.com/nothings/stb.git External/stb
+fi
 
 mkdir Build
 cd Build
